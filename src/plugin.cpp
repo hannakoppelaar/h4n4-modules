e3e7c458c0b5b8a1ccc3422b205b9959933fb7f9
Hanna Koppelaar <33447425+hannakoppelaar@users.noreply.github.com>
2022-05-04 16:24:23 +0200

    Typo

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,7 +1,7 @@
 /**
  * Copyright 2022 Hanna Koppelaar
  *
- * This file is part of the h4h4 collection of VCV modules. This collection is free software: you can
+ * This file is part of the h4n4 collection of VCV modules. This collection is free software: you can
  * redistribute it and/or modify it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
